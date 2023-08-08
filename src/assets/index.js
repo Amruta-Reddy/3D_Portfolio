c16c052b674898f150cf1829ed7553a0b6fb53bc
Amruta-Reddy <115242296+Amruta-Reddy@users.noreply.github.com>
2023-08-08 09:01:08 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,51 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import shellscipt from "./tech/shellscipt.png";
+import threejs from "./tech/threejs.svg";
+
+import crud from './crud.jpg';
+import wordpress from "./wordpress.jpg";
+import tictactoe from "./tictactoe.png";
+import todolist from "./todolist.png";
+import calci from "./calci.png";
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  reactjs,
+  tailwind,
+  shellscipt,
+  threejs,
+  crud,
+  wordpress,
+  tictactoe,
+  todolist,
+  calci,
+
+};
